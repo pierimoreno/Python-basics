141160a2ba26c2160a2c77d3273df9005180e88e
Pierina Moreno <70977664+pierimoreno@users.noreply.github.com>
2020-10-12 20:35:41 -0700

    class AnonymousSurvey:

---
+++ survey.py
@@ -0,0 +1,24 @@
+class AnonymousSurvey:
+    """Collect anonymous answers to a survey question"""
+
+    def __init__(self, question):
+        """Store a question, and prepare to store responses"""
+        self.question = question
+        self.responses = []
+
+    def show_question(self):
+        """show the survey question"""
+        print(self.question)
+
+    def store_responses(self, new_response):
+        """store a single response to the survey"""
+        self.responses.append(new_response)
+
+    def show_results(self):
+        """show all the responses that have users given"""
+        print(f"Here is the survey results: ")
+        for response in self.responses:
+            print(f"{response}")
+
+
+
